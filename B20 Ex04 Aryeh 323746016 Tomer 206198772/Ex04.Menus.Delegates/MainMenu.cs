49830c5db4c4aca889a317ed89885646c55ae507
aryehlev <59808808+aryehlev@users.noreply.github.com>
2020-06-22 00:19:47 +0300

    a

---
+++ B20 Ex04 Aryeh 323746016 Tomer 206198772/Ex04.Menus.Delegates/MainMenu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Ex04.Menus.Delegates
+{
+    class MainMenu
+    {
+        private MenuItem m_RootOfMenus;
+
+        public MainMenu(string i_Title)
+        {
+            m_RootOfMenus = new MenuItem(i_Title, null, null, 0, false);
+        }
+
+        public MenuItem RootOfMenus { get;}
+
+        public void Show()
+        {
+            MenuItem currentMenuBeingShown = RootOfMenus;
+            while(true)
+            {
+                buildMenu(currentMenuBeingShown);
+                if(checkIfGoodInput(out string inputFromUser))
+                {
+                    MenuItem menuItemPicked = getSubMenuFromUsr(currentMenuBeingShown, inputFromUser, out bool wantsToGoBack);
+                    if(wantsToGoBack)
+                    {
+                        if(currentMenuBeingShown == RootOfMenus)
+                        {
+                            //print goodbye
+                            break;
+                        }
+                        else
+                        {
+                            currentMenuBeingShown = currentMenuBeingShown.FatherItem;
+                            continue;
+                        }
+                    }
+
+                    if(menuItemPicked.IsFinal)
+                    {
+                        ///do action
+                    }
+                    else
+                    {
+                        currentMenuBeingShown = menuItemPicked;
+                    }
+                }
+            }
+        }
+        
+        private void buildMenu(MenuItem i_MenuItemToShow)
+        {
+
+        }
+
+        private MenuItem getSubMenuFromUsr(MenuItem i_CurrentMenuItem, string i_NumOfSubMenuFromUser, out bool i_WantsToGoBack)
+        {
+            i_WantsToGoBack = false;
+            return null;
+        }
+
+        private bool checkIfGoodInput(out string o_ErrorMsg)
+        {
+            o_ErrorMsg = "";
+            return true;
+        }
+    }
+}
---
+++ B20 Ex04 Aryeh 323746016 Tomer 206198772/Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex04.Menus.Delegates
+{ 
+    class MenuItem
+    {
+        private readonly string r_Title;
+        private List<MenuItem> m_SubMenuItems;
+        private readonly MenuItem r_FatherItem;
+        private readonly bool r_IsFinal;
+        public event Action FinalItemWasChosen;
+        private readonly int m_Level;
+        
+        public MenuItem(string i_Title, List<MenuItem> i_SubMenuItems, MenuItem i_FatherItem, int i_Level, bool i_IsFinal)
+        {
+            r_Title = i_Title;
+            m_SubMenuItems = i_SubMenuItems;
+            r_FatherItem = i_FatherItem;
+            m_Level = i_Level;
+            r_IsFinal = i_IsFinal;
+        }
+
+        public string Title
+        {  
+            get
+            {
+                return r_Title;
+            }
+        }
+        public bool IsFinal
+        {
+            get
+            {
+                return r_IsFinal;
+            }
+        }
+
+        public MenuItem FatherItem
+        {
+            get
+            {
+                return r_FatherItem
+            }
+        }
+
+        public bool TryAddMainMenuItem(string i_Title, bool i_ItemToAddIsFinal, out MenuItem i_MenuItemAdded, Action i_ActionOptionalIfFinal = null)
+        {
+            bool wasSuccess = false;
+            i_MenuItemAdded = null;
+
+            if (!this.r_IsFinal)
+            {
+                MenuItem menuItemToAdd = new MenuItem(i_Title, null, this, this.m_Level + 1, i_ItemToAddIsFinal);
+                m_SubMenuItems.Add(menuItemToAdd);
+                i_MenuItemAdded = menuItemToAdd;
+                wasSuccess = true;
+            }
+
+            if(i_ItemToAddIsFinal)
+            {
+                i_MenuItemAdded.FinalItemWasChosen += i_ActionOptionalIfFinal;
+            }
+
+            return wasSuccess;
+        } 
+        
+        protected virtual void OnFinalItemWasChosen()
+        {
+            FinalItemWasChosen?.Invoke();
+        }
+    }
+}
