d8a4f092a2ad57c275c2a73677da830271bf3fc3
Tomer <tomer2911@gmail.com>
2020-06-22 19:52:23 +0300

    AAAAAAAAABBBBBBBBBCCCCCCDDDDDDDDEEEEEEEEFFFF

---
+++ B20 Ex04 Aryeh 323746016 Tomer 206198772/Ex04.Menus.Test/TestMenuWithDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Ex04.Menus.Delegates;
+
+namespace Ex04.Menus.Test
+{
+    class TestMenuWithDelegates
+    {
+        public static void ShowTest()
+        {
+            MainMenu mainMenu = new MainMenu("Menu With Delegates");
+            mainMenu.TryAddMenuItem("Version and Capitals");
+            mainMenu.TryAddMenuItem("Show Date/Time");
+            mainMenu.TraverseDown(0);
+            mainMenu.TryAddMenuItem("Count Capitals", print);
+            mainMenu.TryAddMenuItem("Show Version", print);
+            mainMenu.TraverseUp();
+            mainMenu.TraverseDown(1);
+            mainMenu.TryAddMenuItem("Show Time", print);
+            mainMenu.TryAddMenuItem("Show Date", print);
+            mainMenu.Show();
+        }
+
+        public static void print()
+        {
+            Console.Out.WriteLine("123");
+        }
+    }
+}
---
+++ B20 Ex04 Aryeh 323746016 Tomer 206198772/Ex04.Menus.Test/TestMenuWithInterfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Ex04.Menus.Interfaces;
+
+namespace Ex04.Menus.Test
+{
+    class TestMenuWithInterfaces
+    {
+        public static void ShowTest()
+        {
+            MainMenu mainMenu = new MainMenu("Menu With Interfaces");
+            mainMenu.TryAddNonExecutableMenuItem("Version and Capitals");
+            mainMenu.TryAddNonExecutableMenuItem("Show Date/Time");
+            mainMenu.TraverseDown(0);
+            mainMenu.TryAddExecutableMenuItem("Count Capitals", print);
+            mainMenu.TryAddExecutableMenuItem("Show Version", print);
+            mainMenu.TraverseUp();
+            mainMenu.TraverseDown(1);
+            mainMenu.TryAddExecutableMenuItem("Show Time", print);
+            mainMenu.TryAddExecutableMenuItem("Show Date", print);
+            mainMenu.Show();
+        }
+
+        public static void print()
+        {
+            Console.Out.WriteLine("123");
+        }
+    }
+}
